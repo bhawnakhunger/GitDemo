5b557580cff43922db2d428d439fded75ed160e9
bhawnakhunger <bhawna.khunger@gmail.com>
2020-05-14 00:26:43 +0530

    Updated Commit

--- src/test/java/com/company/RestAPITest.java
+++ src/test/java/com/company/RestAPITest.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class RestAPITest {
     @Test
     public void Postjira(){
-        System.out.println("PostJira");
+        System.out.println("PostJira1");
+        System.out.println("PostJira2");
+        System.out.println("PostJira3");
     }
 
     @Test
